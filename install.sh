17493bd9c9544ece42bcd2a9f4b763292e2e72f5
Rasmus Leupold <rasmus.leupold@check24.de>
2020-05-19 23:05:59 +0200

    rename var to user_bin_path

--- install.sh
+++ install.sh
@@ -2,17 +2,17 @@
 
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
-bin_user_path=$HOME/bin
+user_bin_path=$HOME/bin
 
-if ! [[ -d $bin_user_path ]]; then
-  echo "'$bin_user_path/' does not exists"
+if ! [[ -d $user_bin_path ]]; then
+  echo "'$user_bin_path/' does not exists"
   exit 1
 fi
 
-echo $PATH | grep $bin_user_path > /dev/null
+echo $PATH | grep $user_bin_path > /dev/null
 
 if ! [[ $? -eq 0 ]]; then
-  echo "'$bin_user_path' is not in your PATH variable"
+  echo "'$user_bin_path' is not in your PATH variable"
   echo "ERROR: installation failed"
   exit 1
 fi
@@ -20,17 +20,17 @@ fi
 for tool_script in $(find $(pwd)/tools -name '*.sh'); do
   command_str=$(echo $(basename $tool_script) | cut -d . -f 1)
 
-  ln -s $tool_script $bin_user_path/$command_str 2> /dev/null
+  ln -s $tool_script $user_bin_path/$command_str 2> /dev/null
 
   if ! [[ $? -eq 0 ]]; then
-    if [[ -L $bin_user_path/$command_str ]]; then
-      if [[ $(readlink $bin_user_path/$command_str) ==  $tool_script ]]; then
+    if [[ -L $user_bin_path/$command_str ]]; then
+      if [[ $(readlink $user_bin_path/$command_str) ==  $tool_script ]]; then
         echo "link for '$command_str' already exists"
       else
         echo "link for '$command_str' already exists, but point to a different file"
       fi
     else
-      echo "there is da different file with the same name of '$command_str' in the '$bin_user_path/' directory"
+      echo "there is da different file with the same name of '$command_str' in the '$user_bin_path/' directory"
     fi
   else
     echo "link for '$command_str' created"
