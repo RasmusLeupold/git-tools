0b891e63e1c69e2a638cffd6bd2354150b859418
Rasmus Leupold <rasmus.leupold@check24.de>
2020-05-19 16:18:10 +0200

    update bash tests in script

--- tools/gcob.sh
+++ tools/gcob.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-if [[ $# -ne 1 ]]; then
+if [ $# -ne 1 ]; then
   git branch
   exit 0
 fi
@@ -9,7 +9,7 @@ brach_name=$1
 
 number_of_branches=$(git branch | grep $brach_name | awk '{print $1}' | wc -l)
 
-if [[ $number_of_branches -gt 1 ]]; then
+if [ $number_of_branches -gt 1 ]; then
   echo "  [more] then one branch matches"
   echo ""
   echo "  which one are you looking for:"
@@ -18,7 +18,7 @@ if [[ $number_of_branches -gt 1 ]]; then
   exit 0
 fi
 
-if [[ $number_of_branches -eq 0 ]]; then
+if [ $number_of_branches -eq 0 ]; then
     echo "  [could not] find a matching branch"
     echo ""
     echo "  current local branches:"
