e7380e680ed101aa77a44de4aee887ab7a3f5722
Rasmus Leupold <rasmus.leupold@check24.de>
2020-10-01 17:19:53 +0200

    add the gchb script

---
+++ tools/gchb.sh
@@ -0,0 +1,25 @@
+#/bin/bash
+
+branches=( $(git branch | sed '/^*/d' | tr -d '\n') )
+i=1
+
+for branch in "${branches[@]}"; do
+  echo "$i: $branch"
+  ((i=i+1))
+done
+
+((i=i-1))
+
+while [ -z $branch_number ] || ! [ $branch_number -eq $branch_number ] 2>/dev/null || ! [ $branch_number -le $i ]; do
+  read -p "number of the branch: " branch_number
+
+  if [ -n $branch_number ] && ! [ $branch_number -eq $branch_number ] 2>/dev/null; then
+    echo "put in a number, plz"
+  else
+    [ $branch_number -le $i ] || echo "put in a number between 1 and $i, plz"
+  fi
+done
+
+((branch_number=branch_number-1))
+
+git checkout ${branches[$branch_number]}
