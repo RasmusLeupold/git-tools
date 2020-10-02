dbbcce1420da2d7246b6c4344a5b94ce480f5af6
Rasmus Leupold <rasmus.leupold@check24.de>
2020-10-02 09:43:17 +0200

    improve on printing out text for gchb

--- tools/gchb.sh
+++ tools/gchb.sh
@@ -11,12 +11,12 @@ done
 [ $i -eq 0 ] && echo "There is only the current local branch." && exit 0
 
 while [ -z $branch_number ] || ! [ $branch_number -eq $branch_number ] 2>/dev/null || ! [ $branch_number -le $i ]; do
-  read -p "number of the branch: " branch_number
+  read -p "Number of the branch to switch to: " branch_number
 
   if [ -n $branch_number ] && ! [ $branch_number -eq $branch_number ] 2>/dev/null; then
-    echo "put in a number, plz"
+    echo "Please put in a number between 1 and $i!"
   else
-    [ $branch_number -le $i ] || echo "put in a number between 1 and $i, plz"
+    [ $branch_number -le $i ] || echo "Please put in a number between 1 and $i!"
   fi
 done
 
