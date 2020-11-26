141cd2831190f80ba3ccd061b4562b2bf7b30517
Rasmus Leupold <rasmus.leupold@check24.de>
2020-11-26 12:15:49 +0100

    remive punctuation characters

--- tools/gchb.sh
+++ tools/gchb.sh
@@ -8,15 +8,15 @@ for branch in "${branches[@]}"; do
   echo "$i: $branch\n"
 done
 
-[ ${#branches[@]} -eq 0 ] && echo "There is no or only the current local feature branch." && exit 0
+[ ${#branches[@]} -eq 0 ] && echo "There is no or only the current local feature branch" && exit 0
 
 while [ -z $branch_number ] || ! [ $branch_number -eq $branch_number ] 2>/dev/null || ! [ $branch_number -le $i ]; do
   read -p "Number of the branch to switch to: " branch_number
 
   if [ -n $branch_number ] && ! [ $branch_number -eq $branch_number ] 2>/dev/null; then
-    echo "Please put in a number between 1 and $i!"
+    echo "Please put in a number between 1 and $i"
   else
-    [ $branch_number -le $i ] || echo "Please put in a number between 1 and $i!"
+    [ $branch_number -le $i ] || echo "Please put in a number between 1 and $i"
   fi
 done
 
