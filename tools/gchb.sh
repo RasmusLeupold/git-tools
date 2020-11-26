63e536181450c97255a7920755d2ab840fe7d57a
Rasmus Leupold <rasmus.leupold@check24.de>
2020-11-26 13:51:18 +0100

    enable checkout branches by pattern

--- tools/gchb.sh
+++ tools/gchb.sh
@@ -10,15 +10,34 @@ done
 
 [ ${#branches[@]} -eq 0 ] && echo "There is no or only the current local feature branch" && exit 0
 
-while [ -z $branch_number ] || ! [ $branch_number -eq $branch_number ] 2>/dev/null || ! [ $branch_number -le $i ]; do
-  read -p "Number of the branch to switch to: " branch_number
+while [ -z $branch_found ]; do
+  read -p "Please put in a number of a branch or a pattern to search for: " branch_number_or_search_pattern
 
-  if [ -n $branch_number ] && ! [ $branch_number -eq $branch_number ] 2>/dev/null; then
-    echo "Please put in a number between 1 and $i"
+  [ -z $branch_number_or_search_pattern ] && echo "Please make an input"
+
+  if [ -n "$branch_number_or_search_pattern" ]; then
+    if [ $branch_number_or_search_pattern -eq $branch_number_or_search_pattern ] 2>/dev/null; then
+      if [ $branch_number_or_search_pattern -le $i ]; then
+        branch_index=$((branch_number_or_search_pattern-1))
+      else
+        number_of_matching_branches=$(git branch | grep $branch_number_or_search_pattern | awk '{print $1}' | wc -l)
+      fi
+    else
+      number_of_matching_branches=$(git branch | grep $branch_number_or_search_pattern | awk '{print $1}' | wc -l)
+    fi
+  fi
+
+  if [ -n "$branch_index" ]; then
+    branch_found=true
   else
-    [ $branch_number -le $i ] || echo "Please put in a number between 1 and $i"
+    if [ -n "$number_of_matching_branches" ]; then
+      [ $number_of_matching_branches -eq 0 ] && echo "there is no matching branch"
+      [ $number_of_matching_branches -gt 1 ] && echo "there is more than one matching branch"
+      [ $number_of_matching_branches -eq 1 ] && branch_search_pattern=$branch_number_or_search_pattern && branch_found=true
+      number_of_matching_branches=""
+    fi
   fi
 done
 
-branch_index=$((branch_number-1))
-git checkout ${branches[$branch_index]}
+[ -n "$branch_search_pattern" ] && git branch | grep $branch_search_pattern | awk '{print $1}' | xargs -n1 git checkout
+[ -n "$branch_index" ] && git checkout ${branches[$branch_index]}
