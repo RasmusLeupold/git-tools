f46ede1b7cf60a61c5c997b45602e6c70e2e777f
Rasmus Leupold <rasmus.leupold@check24.de>
2022-01-14 14:41:01 +0100

    add new skript to show and delete gone branches

---
+++ tools/git-delete-gone-branches.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+git remote prune origin > /dev/null
+
+gone_branches=( $(git branch -v | sed '/^*/d' | awk '{print $3 " " $1}' | grep '^\[gone\]' | awk '{print $2 }') )
+
+if [ ${#gone_branches[@]} -eq 0 ]; then
+  echo "There is no branch to be deleted which is gone on the remote"
+  exit 0
+fi
+
+( IFS=$'\n'; echo "${gone_branches[*]}" )
+
+read -p $'\n'"Are you sure to delete branches listed above (y/N): " confirm
+
+if [[ "$confirm" =~ ^(Y|y|yes|Yes|YES)$ ]]; then
+  echo ${gone_branches[@]} | sed 's/ /\n/g' | xargs git branch -D
+fi
