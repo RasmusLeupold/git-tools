791b74c299e3f707546e7d6f4424cba6575fd35a
Rasmus Leupold <rasmus.leupold@check24.de>
2022-01-17 07:46:46 +0100

    shorten no branch message

--- tools/git-delete-gone-branches.sh
+++ tools/git-delete-gone-branches.sh
@@ -5,7 +5,7 @@ git remote prune origin > /dev/null
 gone_branches=( $(git branch -v | sed '/^*/d' | awk '{print $3 " " $1}' | grep '^\[gone\]' | awk '{print $2 }') )
 
 if [ ${#gone_branches[@]} -eq 0 ]; then
-  echo "There is no branch to be deleted which is gone on the remote"
+  echo "There is no gone branch to be deleted"
   exit 0
 fi
 
