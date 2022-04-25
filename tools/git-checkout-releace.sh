de394ada00ad044f302e7689293e05f8ea9523ce
Rasmus Leupold <rasmus.leupold@check24.de>
2022-04-25 11:21:47 +0200

    Fix regexs

--- tools/git-checkout-releace.sh
+++ tools/git-checkout-releace.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+
 display_error () {
   echo "ERROR: $1"
   exit 1
 }
 
 if [[ -n $1 ]]; then
-  if [[ "$1" =~ ^-$|^[0-9]+$ ]]; then
+  if [[ "$1" =~ ^-$|^[1-9][0-9]*$ ]]; then
     [[ "$1" =~ ^-$ ]] || version_number=$1
   else
     display_error "Your first argument was not a valid version number"
@@ -13,8 +14,8 @@ if [[ -n $1 ]]; then
 fi
 
 if [[ -n $2 ]]; then
-  if [[ "$2" =~ ^-$|^-[0-9]+$|^[0-9]+$ ]]; then
-    [[ "$2" =~ ^-$ ]] || delta_days=$2
+  if [[ "$2" =~ ^-[1-9][0-9]*$|^[1-9][0-9]*$ ]]; then
+    [[ "$2" =~ ^-[1-9][0-9]*$ ]] && delta_days=$2 || delta_days="+$2"
   else
     display_error "Your second argument was not a valid delta of days"
   fi
